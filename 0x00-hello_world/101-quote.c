38ea484770accfe22abb7ca689d6601f1e73d3cd
innthomas2 <innthomas@tutanota.com>
2023-03-16 13:26:05 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+int main(void) {
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    const size_t length = strlen(message);
+    const ssize_t bytes_written = write(2, message, length);
+    return bytes_written != length;
 }
